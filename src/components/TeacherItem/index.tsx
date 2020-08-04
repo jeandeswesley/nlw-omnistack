5734995736fdf6e38ff2a282a6faec1f672a6ef1
Jeandes <jeandes@iMac-de-Jeandes.local>
2020-08-03 23:35:43 -0300

    Create home, teacher list and teacher form partial

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css'
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+          <header>
+            <img src="https://pbs.twimg.com/profile_images/1018669027495948289/X6gVzkiK_400x400.jpg" />
+          <div>
+            <strong>Jeandes Wesley</strong>
+            <span>React</span>
+          </div>
+          </header>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          <br /><br />
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+  )
+}
+
+export default TeacherItem;
